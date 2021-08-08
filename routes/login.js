ff8a2509cd4c92add903848c8943cb3194c32cba
marckesin <192.168.2.1@protonmail.com>
2021-08-08 15:13:07 -0300

    adicionado meta description

--- routes/login.js
+++ routes/login.js
@@ -4,17 +4,17 @@ const User = require('../models/user.model');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('login');
+    res.render('login', { info: "" });
 });
 
 router.post('/', async (req, res) => {
-  await User.findOne({ email: req.body.username, password: md5(req.body.password) }, (err, result) => {
-    if (!err && result) {
-      res.render('secrets', { secret: result.password });
-    } else {
-      res.redirect('/login');
-    }
-  });
+    await User.findOne({ email: req.body.username, password: md5(req.body.password) }, (err, result) => {
+        if (!err && result) {
+            res.render('secrets', { secret: result.password });
+        } else {
+            res.render('login', { info: "Email ou senha inválidos!" });
+        }
+    });
 });
 
 module.exports = router;
